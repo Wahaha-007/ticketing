1377e79d32605ff4ffe3e1f27434bca4c7d15837
Mana M <b0464221@gmail.com>
2022-06-15 20:04:30 +0700

    Finish 'Current User' route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,8 +4,17 @@ import jwt from 'jsonwebtoken';
 const router = express.Router(); // Special Object that contains Routes
 
 router.get('/api/users/currentuser', (req, res) => {
+  // 1. No cookie at all
   if (!req.session?.jwt) {
     // equivalent to (!req.session || !req.session.jwt)
+    return res.send({ currentUser: null });
+  }
+
+  // 2. Use standard library to verify JWT
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (err) {
     return res.send({ currentUser: null });
   }
 });
