0988a7e5e7e4f94b8fd6c4f8a4c6b064c98a59a0
Wahaha <b0464221@gmail.com>
2022-06-19 14:08:27 +0700

    Add SignUp page

---
+++ client/hooks/use-request.js
@@ -0,0 +1,35 @@
+// We are now making custom Hook here
+
+import { useState } from 'react';
+import axios from 'axios';
+
+export default ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        // No need response.data, but just provide for maybelater use
+        onSuccess(response.data);
+      }
+
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Ooops....</h4>
+          <ul className="my-0">
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      ); // Remember this format !
+    }
+  };
+
+  return { doRequest, errors };
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,19 @@
-const banana = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>;
 };
 
-export default banana;
+// Special function by NextJS
+// LandingPage.getInitialProps = async () => {
+//   const response = await axios.get('/api/users/currentuser');
+
+//   return response.data;
+// };
+
+export default LandingPage;
