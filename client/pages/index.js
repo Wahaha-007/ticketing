8aae3e93066f60852be86d663318a0bd39a61b5a
Wahaha <b0464221@gmail.com>
2022-06-19 20:26:50 +0700

    Make NextJS Client reach Signup/api/users/xxxx

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,36 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
   console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
 
   return <h1>Landing Page</h1>;
 };
 
-// Special function by NextJS
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async ({ req }) => {
+  // console.log(req.headers);
+  if (typeof window === 'undefined') {
+    // we are on the server!
 
-//   return response.data;
-// };
+    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        // Nginx also needs domain name information !
+        // headers: { Host: 'ticketing.dev' },
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser'); // response.data
+
+    return data;
+  }
+};
 
 export default LandingPage;
