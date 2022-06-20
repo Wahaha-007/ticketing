60472d2c6b7e1a06a6207e26e16f7118be105d62
Wahaha <b0464221@gmail.com>
2022-06-20 08:15:54 +0700

    Add SignIn page

---
+++ client/api/build-client.js
@@ -0,0 +1,19 @@
+// This helper function is made in order to make index.js more tidy !
+import axios from 'axios';
+
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
+    // We are on the server
+
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    // We must be on the browser
+    return axios.create({
+      baseUrl: '/',
+    });
+  }
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,36 +1,42 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser');
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  // console.log(req.headers);
-  if (typeof window === 'undefined') {
-    // we are on the server!
+// LandingPage.getInitialProps = async ({ req }) => {
+//   if (typeof window === 'undefined') {
+//     // we are on the server!
 
-    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        // Nginx also needs domain name information !
-        // headers: { Host: 'ticketing.dev' },
-        headers: req.headers,
-      }
-    );
+//     // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
+//     const { data } = await axios.get(
+//       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+//       {
+//         // Nginx also needs domain name information !
+//         // headers: { Host: 'ticketing.dev' },
+//         headers: req.headers,
+//       }
+//     );
 
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser'); // response.data
+//     return data;
+//   } else {
+//     // we are on the browser!
 
-    return data;
-  }
+//     const { data } = await axios.get('/api/users/currentuser'); // response.data
+
+//     return data;
+//   }
+// };
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
 };
 
 export default LandingPage;
