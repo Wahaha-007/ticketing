371e5717956733afc90ece7f5c7eaeb4ef95dc4e
Wahaha <b0464221@gmail.com>
2022-06-27 12:15:17 +0700

    Complete Listener and Publisher, ready to make npm module

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,6 +1,6 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
-import { TicketCreatedEvent } from './ticket-created-events';
+import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
 
 class TicketCreatedListener extends Listener<TicketCreatedEvent> {
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -7,17 +8,28 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '1234',
-    title: 'concert',
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '1234',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
-  // 3rd param = callback after publish data
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  // const data = JSON.stringify({
+  //   id: '1234',
+  //   title: 'concert',
+  //   price: 20,
+  // });
+
+  // // 3rd param = callback after publish data
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
