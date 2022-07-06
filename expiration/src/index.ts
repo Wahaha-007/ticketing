9e16ad8f11e7af5b74fec821fa5c4ac9ace6c761
Wahaha <b0464221@gmail.com>
2022-07-06 18:14:31 +0700

    Start expiration service structure

---
+++ expiration/src/index.ts
@@ -0,0 +1,44 @@
+import { natsWrapper } from './nats-wrapper';
+
+// 1.2.3 Was moved to app.ts as the part of refactoring for 'Test' preparation
+// 4. Real Working Function
+
+const start = async () => {
+  // -------- ENV Preparing section -------- //
+  // ( Must do this for every ENV var to supress Typescript error)
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  // -------- Real running base section -------- //
+  try {
+    // 4.2 Connect to NATS
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on('close', () => {
+      // post close message
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    // Graceful Shutdown - Formally 'Close' connection
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
