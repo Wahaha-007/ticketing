9b1a6a92759fd9c7517d6b6154c8596061993d2b
Wahaha <b0464221@gmail.com>
2022-07-12 00:19:40 +0700

    Start Client Service again, fix currentUser props data passing

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -10,7 +10,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
   );
 };
@@ -23,7 +23,11 @@ AppComponent.getInitialProps = async (appContext) => {
   // --------- Additional task that _app.js work for eacha page --------
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   // -------------------------------------------------------------------
